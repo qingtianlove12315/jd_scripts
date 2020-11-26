1398de15388379948868f9854a71b7997c9443dc
darkedge <luojiehong@darkedge.cn>
2020-11-26 20:14:47 +0800

    🐬

--- share_code_upload.js
+++ share_code_upload.js
@@ -1,6 +1,5 @@
-
-// request 种豆提交互助码api 
-function requestZD(token) {
+// 互助请求
+function requestHelper(action, token) {
   (function (callback) {
     'use strict';
 
@@ -9,7 +8,7 @@ function requestZD(token) {
     const httpOptions = {
       hostname: 'api.turinglabs.net',
       port: '80',
-      path: `/api/v1/jd/bean/create/${token}/`,
+      path: `/api/v1/jd/${action}/create/${token}/`,
       method: 'GET',
       headers: { 'Content-Type': 'text/palin; charset=utf-8', "Accept-Encoding": "zlib, deflate, zstd, br" }
     };
@@ -45,128 +44,33 @@ function requestZD(token) {
 
 
   })((error, statusCode, headers, body) => {
-    console.log(`STATUS:${statusCode} BODY:${body}`);
+    console.log(`${action} STATUS:${statusCode} BODY:${body}`);
   });
 }
 
-// request 农场提交互助码api 
-function requestNC(token) {
-  (function (callback) {
-    'use strict';
-
-    const httpTransport = require('http');
-    const responseEncoding = 'utf8';
-    const httpOptions = {
-      hostname: 'api.turinglabs.net',
-      port: '80',
-      path: `/api/v1/jd/farm/create/${token}/`,
-      method: 'GET',
-      headers: { 'Content-Type': 'text/palin; charset=utf-8', "Accept-Encoding": "zlib, deflate, zstd, br" }
-    };
-    httpOptions.headers['User-Agent'] = 'node ' + process.version;
-
-    // Paw Store Cookies option is not supported
-
-    const request = httpTransport.request(httpOptions, (res) => {
-      let responseBufs = [];
-      let responseStr = '';
-
-      res.on('data', (chunk) => {
-        if (Buffer.isBuffer(chunk)) {
-          responseBufs.push(chunk);
-        }
-        else {
-          responseStr = responseStr + chunk;
-        }
-      }).on('end', () => {
-        responseStr = responseBufs.length > 0 ?
-          Buffer.concat(responseBufs).toString(responseEncoding) : responseStr;
-
-        callback(null, res.statusCode, res.headers, responseStr);
-      });
-
-    })
-      .setTimeout(0)
-      .on('error', (error) => {
-        callback(error);
-      });
-    request.write("")
-    request.end();
-
-
-  })((error, statusCode, headers, body) => {
-    console.log(`STATUS:${statusCode} BODY:${body}`);
-  });
-
-}
-// request 萌宠提交互助码api jhc 
-function requestMC(token) {
-  (function (callback) {
-    'use strict';
-
-    const httpTransport = require('http');
-    const responseEncoding = 'utf8';
-    const httpOptions = {
-      hostname: 'api.turinglabs.net',
-      port: '80',
-      path: `/api/v1/jd/pet/create/${token}/`,
-      method: 'GET',
-      headers: { 'Content-Type': 'text/palin; charset=utf-8', "Accept-Encoding": "zlib, deflate, zstd, br" }
-    };
-    httpOptions.headers['User-Agent'] = 'node ' + process.version;
-
-    // Paw Store Cookies option is not supported
-
-    const request = httpTransport.request(httpOptions, (res) => {
-      let responseBufs = [];
-      let responseStr = '';
-
-      res.on('data', (chunk) => {
-        if (Buffer.isBuffer(chunk)) {
-          responseBufs.push(chunk);
-        }
-        else {
-          responseStr = responseStr + chunk;
-        }
-      }).on('end', () => {
-        responseStr = responseBufs.length > 0 ?
-          Buffer.concat(responseBufs).toString(responseEncoding) : responseStr;
-
-        callback(null, res.statusCode, res.headers, responseStr);
-      });
-
-    })
-      .setTimeout(0)
-      .on('error', (error) => {
-        callback(error);
-      });
-    request.write("")
-    request.end();
-
-
-  })((error, statusCode, headers, body) => {
-    // console.log('ERROR:', error);
-    // console.log('STATUS:', statusCode);
-    // console.log('HEADERS:', JSON.stringify(headers));
-    // console.log('BODY:', body);
-    console.log(`STATUS:${statusCode} BODY:${body}`);
-  });
+// 数据分割
+function getSpliteList(code) {
+  if (code.indexOf('&') > -1) {
+    return code.split('&');
+  } else if (code.indexOf('\n') > -1) {
+    return code.split('\n');
+  } else {
+    return code.split();
+  }
+  return [];
 }
 
-let ZDList = [
-  'uxlqtimyc6trkvrkcs6cn4y4ma',
-  'r2kjupw2sq3ix766qqovwvxuzi',
-  'e7lhibzb3zek3cjd57phorfifpskoh5mvajzrea',
-  'ztnqxc7dlck4z5vgyqpc54p3aq',
-  'hvkoiqb2jlg4ewhabtslfwz33q3h7wlwy7o5jii',
-];
+// 批量请求
+function requsetListWithAction(codeList, action) {
+  codeList.forEach(token => {
+    requestHelper(action, token);
+  });
 
+  console.log(`${action} : ${codeList.join('&')}`);
+}
 
-ZDList.forEach(token => {
-  requestZD(token);
-});
-
-let NCList = [
+// 东东农场
+let FRUITSHARECODES = [
   'f630c68384eb417faba0e0132661e246',
   '5b6026f045684fe0918b978501fd6601',
   '28ca8c7a98a342969f4933c8f7152ed6',
@@ -174,17 +78,70 @@ let NCList = [
   '99dc4d30caf04801a6f8429af6c4d5b7',
 ];
 
-NCList.forEach(token => {
-  requestNC(token);
-});
+// 读取东东农场
+if (process.env.FRUITSHARECODES) {
+  let value = getSpliteList(process.env.FRUITSHARECODES)
+  if (value.length > 0) {
+    FRUITSHARECODES = value
+  }
+}
 
-let MCList = [
-  'MTE1NDUwMTI0MDAwMDAwMDQwMTMwNjA3',
-  'MTE1NDAxNzYwMDAwMDAwNDA4MzI3Njk=',
-  'MTE1NDAxNzcwMDAwMDAwNDEzMzU5Mzc=',
-  'MTE1NDAxNzcwMDAwMDAwMzcxNzkwNTM=',
-  'MTE1NDQ5MzYwMDAwMDAwNDEzNDAwMTk=',
+requsetListWithAction(FRUITSHARECODES, 'ddfactory');
+
+// 东东萌宠
+let PETSHARECODES = [
+  'f630c68384eb417faba0e0132661e246',
+  '5b6026f045684fe0918b978501fd6601',
+  '28ca8c7a98a342969f4933c8f7152ed6',
+  '4a6aecf9438842599a17435ca111dbb5',
+  '99dc4d30caf04801a6f8429af6c4d5b7',
+]
+
+// 读取东东萌宠
+if (process.env.PETSHARECODES) {
+  let value = getSpliteList(process.env.PETSHARECODES)
+  if (value.length > 0) {
+    PETSHARECODES = value
+  }
+}
+
+requsetListWithAction(PETSHARECODES, 'pet');
+
+// 种豆
+let PLANT_BEAN_SHARECODES = [
+  'uxlqtimyc6trkvrkcs6cn4y4ma',
+  'r2kjupw2sq3ix766qqovwvxuzi',
+  'e7lhibzb3zek3cjd57phorfifpskoh5mvajzrea',
+  'ztnqxc7dlck4z5vgyqpc54p3aq',
+  'hvkoiqb2jlg4ewhabtslfwz33q3h7wlwy7o5jii',
 ];
-MCList.forEach(token => {
-  requestMC(token);
-});
+
+// 读取种豆
+if (process.env.PLANT_BEAN_SHARECODES) {
+  let value = getSpliteList(process.env.PLANT_BEAN_SHARECODES)
+  if (value.length > 0) {
+    PLANT_BEAN_SHARECODES = value
+  }
+}
+
+requsetListWithAction(PLANT_BEAN_SHARECODES, 'bean');
+
+// 东东工厂
+let DDFACTORY_SHARECODES = [
+  'P04z54XCjVWnYaS5mlSUGL63n9DnBo',
+  'P04z54XCjVWnYaS5mlOQT-mijUTkwzH',
+  'P04z54XCjVWnYaS5m9cZ2Tw2y1Pl3kTXKpKABg',
+  'P04z54XCjVWnYaS5nxRQjmmiCcV',
+  'P04z54XCjVWnYaS5jQLAGXx3XtInNv94W4',
+];
+
+
+// 读取种豆
+if (process.env.DDFACTORY_SHARECODES) {
+  let value = getSpliteList(process.env.DDFACTORY_SHARECODES)
+  if (value.length > 0) {
+    DDFACTORY_SHARECODES = value
+  }
+}
+
+requsetListWithAction(DDFACTORY_SHARECODES, 'ddfactory');
