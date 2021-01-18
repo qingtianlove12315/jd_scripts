61272bad887c96eb2a0b95f739956676614b92ee
darkedge <39146020@qq.com>
2021-01-18 11:00:23 +0800

    添加助力码

--- jd_nian.js
+++ jd_nian.js
@@ -46,8 +46,7 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const inviteCodes = [
-  `cgxZaDXWZPCmiUa2akPVmFMI27K6antJzucULQPYNim_BPEW1Dwd@cgxZdTXtIrPYuAqfDgSpusxr97nagU6hwFa3TXxnqM95u3ib-xt4nWqZdz8@cgxZdTXtIO-O6QmYDVf67KCEJ19JcybuMB2_hYu8NSNQg0oS2Z_FpMce45g@cgxZdTXtILiLvg7OAASp61meehou4OeZvqbjghsZlc3rI5SBk7b3InUqSQ0@cgxZ9_MZ8gByP7FZ368dN8oTZBwGieaH5HvtnvXuK1Epn_KK8yol8OYGw7h3M2j_PxSZvYA`,
-  `cgxZaDXWZPCmiUa2akPVmFMI27K6antJzucULQPYNim_BPEW1Dwd@cgxZdTXtIrPYuAqfDgSpusxr97nagU6hwFa3TXxnqM95u3ib-xt4nWqZdz8@cgxZdTXtIO-O6QmYDVf67KCEJ19JcybuMB2_hYu8NSNQg0oS2Z_FpMce45g@cgxZdTXtILiLvg7OAASp61meehou4OeZvqbjghsZlc3rI5SBk7b3InUqSQ0@cgxZdTXtIumO4w2cDgSqvYcqHwjaAzLxu0S371Dh_fctFJtN0tXYzdR7JaY`
+  'cgxZczvaJrjZ6QfADknljE7bzW3LwZ96dggSBWXI@cgxZcyfLe-SNo1fPCALlo3g0Dg4K3rkJ0VSLu9pyAOk@cgxZZjjIfeSPsVGFRGag6WPoTzx7_127Ig@cgxZdTXtILLcuwvLAFT95p1lZLpAA3j52y1GybRVAwWizPrxwl32t6-jACw@cgxZLmKKIbPa7QzADAfGr6ajovHkInbsHcgYiuRtYKDSiSArvA'
 ];
 !(async () => {
   await requireConfig();
